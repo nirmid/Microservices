5a5b371249f02773ef0c9e77d6e125895e5498cd
nirmid <93215733+nirmid@users.noreply.github.com>
2021-12-03 15:19:57 +0200

    started

--- src/main/java/bgu/spl/mics/application/objects/CPU.java
+++ src/main/java/bgu/spl/mics/application/objects/CPU.java
@@ -1,9 +1,30 @@
 package bgu.spl.mics.application.objects;
 
+import java.util.LinkedList;
+
 /**
  * Passive object representing a single CPU.
  * Add all the fields described in the assignment as private fields.
  * Add fields and methods to this class as you see fit (including public methods and constructors).
  */
 public class CPU {
+    private int cores;
+    private LinkedList<DataBatch> data;
+    private Cluster cluster;
+    private long time;
+
+    public CPU(int _cores,LinkedList<DataBatch> _data,Cluster _cluster,int _time){} // constractor
+
+    /**
+     * @pre
+     */
+    public void processData(){} // process the data form linkedlist and send the processed data to cluster
+    public void recieveDataBatch(){} //  add dataBatch to data
+    public void updateTime(int _time){} //  update time according to TimerService
+    public int getCores(){return 1;} // returns cores
+    public LinkedList<DataBatch> getData(){return data;} // returns data
+    public Cluster getCluster(){return cluster;}// returns cluster
+    public long getTime(){return time;} // returns time
+
+
 }
---
+++ src/test/java/bgu/spl/mics/application/objects/CPUTest.java
@@ -0,0 +1,25 @@
+package bgu.spl.mics.application.objects;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CPUTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void processData() {
+    }
+
+    @Test
+    public void recieveDataBatch() {
+    }
+
+    @Test
+    public void updateTime() {
+    }
+}
